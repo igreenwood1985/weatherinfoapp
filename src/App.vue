caceeb17f81e33a3fe3d09b97edecf90f7989069
Isaac Greenwood <igreenwoodwfg@gmail.com>
2024-02-29 14:52:14 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <WeatherInfo/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import WeatherInfo from './components/WeatherInfo.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+      WeatherInfo
   }
 }
 </script>
---
+++ src/components/WeatherInfo.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <div class="inner-container">
+        <button @click="GetWeatherData()">Get Data</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data(){
+      return{
+        weather: {}
+      }
+    },
+    methods:{
+      GetWeatherData(){
+        axios.get('https://api.openweathermap.org/data/2.5/weather?lat=6.1128&lon=125.1717&appid=35c443852ab1e61c6354ecd647733a3f').then(
+          response => {
+            console.log(response.data.weather[0])
+          }
+        ).catch(error => {console.log(error)})
+      }
+    }
+}
+</script>
+
+<style>
+.container  {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+
+}
+
+.inner-container {
+    width: 50%;
+    background-color: blue;
+}
+</style>
